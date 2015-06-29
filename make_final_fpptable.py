d2321c704e757cf8f1e244b7f3872e1846c20081
Timothy Morton <tim.morton@gmail.com>
2015-06-29 15:09:20 -0400

    explorting

--- make_final_fpptable.py
+++ make_final_fpptable.py
@@ -8,20 +8,18 @@ from configobj import ConfigObj
 
 from keputils import koiutils as ku
 
-
 from astropy import constants as const
 RSUN = const.R_sun.cgs.value
 REARTH = const.R_earth.cgs.value
 
 fpp_file = 'fpp_all.txt'
-positional_file = 'target_probabilities_high_confidence_02-Jan-2014.txt'
+positional_file = 'positional_probability.csv'
 starprop_file = 'starprops_all.txt'
 
 fpp_table = pd.read_table(fpp_file, delim_whitespace=True)
 fpp_table.index = fpp_table['koi']
 
-positional_table = pd.read_table(positional_file, delim_whitespace=True)
-positional_table.index = positional_table['koi'].apply(ku.koiname)
+positional_table = pd.read_csv(positional_file, index_col=0)
 
 star_table = pd.read_table(starprop_file, delim_whitespace=True)
 star_table.index = star_table['koi']
@@ -30,28 +28,12 @@ kois = fpp_table.index
 
 fpp_table['rp'] = fpp_table.ix[kois, 'rprs'] * star_table.ix[kois, 'radius'] * RSUN/REARTH
 fpp_table['disposition'] = ku.DATA.ix[kois, 'koi_disposition']
-fpp_table['prob_ontarget'] = positional_table.ix[kois, 'prob_target']
+fpp_table['prob_ontarget'] = positional_table.ix[kois, 'pp_host_rel_prob']
+fpp_table['pos_prob_score'] = positional_table.ix[kois, 'pp_host_prob_score']
 fpp_table['not_transitlike'] = ku.DATA.ix[kois, 'koi_fpflag_nt'].astype(bool)
 fpp_table['signficant_secondary'] = ku.DATA.ix[kois, 'koi_fpflag_ss'].astype(bool)
 fpp_table['centroid_offset'] = ku.DATA.ix[kois, 'koi_fpflag_co'].astype(bool)
 fpp_table['ephem_match'] = ku.DATA.ix[kois, 'koi_fpflag_ec'].astype(bool)
-
-
-secdepth = np.zeros(len(kois))
-r_excl = np.zeros(len(kois))
-for i,koi in enumerate(fpp_table.index):
-    try:
-        config = ConfigObj(os.path.join(FPPDIR, ku.koiname(koi), 'fpp.ini'))
-        secdepth[i] = float(config['constraints']['secthresh'])*1e6
-        r_excl[i] = float(config['constraints']['maxrad'])
-    except KeyboardInterrupt:
-        raise
-    except:
-        secdepth[i] = np.nan
-        r_excl[i] = np.nan
-
-fpp_table['secdepth'] = secdepth
-fpp_table['r_excl'] = r_excl
 
 fpp_table.to_csv('fpp_final_table.csv')
 
