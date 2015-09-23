b372fd7cf8597a0c7ce72c90b89fcc173979dc94
Timothy Morton <tim.morton@gmail.com>
2015-09-22 22:33:13 -0400

    added file

---
+++ parse_hammerfile.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pandas as pd
+
+d = {}
+for line in open('app_hammer.txt'):
+    line = line.strip().split('|')
+    try:
+        if line[1] not in d:
+            d[line[1]] = {}
+        if line[3] == 'NULL':
+            val = np.nan
+        else:
+            try:
+                val = float(line[3])
+            except ValueError:
+                val = line[3]
+        d[line[1]][line[2]] = val
+    except:
+        raise
+        print('error with line: {}'.format(line))
+        
+df = pd.DataFrame.from_dict(d, orient='index')
+df.to_csv('positional_probability.csv')
